2ad2880d8f4515d5340be8c05babf6eada4dbac9
ayushach007 <ayushach007@gmail.com>
2024-09-28 18:01:41 +0545

    exception handling added

---
+++ src/exception.py
@@ -0,0 +1,45 @@
+import sys
+
+
+def get_detiled_error_message(error, error_details:sys):
+    '''
+    Function to get detailed error message
+    
+    Args:
+    error_message : str : Error message
+    error_details : str : Error details
+
+    Returns:
+    str : Detailed error message
+    '''
+    _, _, tb = sys.exc_info()
+    file_name = tb.tb_frame.f_code.co_filename
+    line_no = tb.tb_lineno
+    message = str(error)
+
+    return f"An error occurred in {file_name} at line {line_no} with message {message} and details {error_details}"
+
+
+class CustomException(Exception):
+    '''
+    Custom exception class to get detailed error message
+    '''
+    def __init__(self, error_message, error_details:sys):
+        '''
+        Constructor for CustomException class
+
+        Args:
+        error_message : str : Error message
+        error_details : str : Error details
+        '''
+        super().__init__(error_message)
+        self.error_message = get_detiled_error_message(error_message, error_details=error_details)
+
+    def __str__(self):
+        '''
+        Function to get string representation of error message
+
+        Returns:
+        str : Error message
+        '''
+        return self.error_message
