b6c001bb91d5407855dd387eef04c293a3a97050
ayushach007 <ayushach007@gmail.com>
2024-09-28 17:15:03 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+HYPHEN_DOT_E = '-e .'
+
+def get_requirements(file_path: str) -> list:
+    '''
+    Read requirements.txt file and return list of requirements
+
+    Args:
+    file_path: str: path to requirements.txt file
+
+    Returns:
+    list: list of requirements
+    '''
+    with open(file_path, 'r') as f:
+        requirements = f.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPHEN_DOT_E in requirements:
+            requirements.remove(HYPHEN_DOT_E)
+
+    return requirements
+
+setup(
+    name='mba_classification',
+    version='0.1',
+    description='A package for classification of MBA students',
+    author='ayushach007',
+    author_email='ayushach007@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
